52f1a0a20708a03662f57bff7d83819a9d842bec
8845musign <8845musign@gmail.com>
2017-12-18 00:44:26 +0900

    add aria status for row selection

--- src/Grid/Row.js
+++ src/Grid/Row.js
@@ -33,7 +33,7 @@ export default class Row extends React.Component {
     })
 
     return (
-      <div className={css} id={`${this.state.rowId}`} role="row">
+      <div className={css} id={`${this.state.rowId}`} role="row" aira-selected={`${this.props.row.checked}`}>
         {this.props.columns.map(this.renderCell.bind(this))}
       </div>
     )
